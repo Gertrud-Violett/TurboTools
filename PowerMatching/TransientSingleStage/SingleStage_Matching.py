0f8c44a8178f60f7d4a7bdff694f80320d079b65
Trude <96937287+Gertrud-Violett@users.noreply.github.com>
2022-08-24 17:59:40 +0900

    Beta ver 0.1 Released
    Working Ver 0.1

--- PowerMatching/TransientSingleStage/SingleStage_Matching.py
+++ PowerMatching/TransientSingleStage/SingleStage_Matching.py
@@ -22,9 +22,10 @@ if __name__ == '__main__':
     setting = configparser.ConfigParser()
     setting.optionxform = str  #Case sensitve option
     setting.read(setting_file, encoding='utf8')
-    params_file = './Inputs/params.toml'
+    params_file = './Inputs/' + setting.get("Settings", "Input filename")
     with open(params_file) as inputf:
         params = toml.load(inputf)
+    initrpm = setting.getfloat("Settings", "Initial rpm")
 
 #Class for each stage element calculation methods
 class Stage:
@@ -63,6 +64,7 @@ class Stage:
 
         self.S_Step=params['Calculation']['Step']
         self.S_PowRes=params['Calculation']['PowRes']
+        self.S_PRCRes=params['Calculation']['PRCRes']
 
 
     def CompStage(self,stgno,Tin,Pin,N_C,W_C,PRC_Target):
@@ -88,13 +90,14 @@ class Stage:
             return Power,N_C,Pout,Tout,PRC,EtaC
 
     def Combustor(self,W_C,Tin,Pin,AFR,C_BLDR,T_BLDR):
-        W_F = W_C*(1-C_BLDR)/AFR
-        W_T = (W_C+W_F)*(1-T_BLDR)
+        W_Comb = W_C*(1-C_BLDR)
+        W_F = W_Comb/AFR
+        W_T = (W_Comb+W_F)*(1-T_BLDR)
         deltaT = self.CB_LHV*10**6*W_F/(self.C_Cp*10**3*W_T)
         Tout = Tin + deltaT
         Pout = Pin*(1-self.CB_dp)
         print('P2c',Pin,'P1t',Pout)
-        return W_T,Pout,Tout
+        return W_F,W_T,Pout,Tout
 
     def TurbStage(self,stgno,EtaM,PwReq,Tin,Pin,N_T,W_T,Pout):
         #Read Turbine Map
@@ -102,30 +105,29 @@ class Stage:
         f_WT = interp2d(x = df['NT'], y = df['PRT'], z = df['WT'])
         f_EtaT = interp2d(x = df['NT'], y = df['PRT'], z = df['EtaT'])
         PRT = Pin/Pout
-        #WT = f_WT(N_T,PRT)
+        WT_Map = f_WT(N_T,PRT)
         EtaT = f_EtaT(N_T,PRT)
         Tadb = Tin*(1-PRT**((self.T_gam-1)/self.T_gam))  #deltaT
         Tout = Tadb*EtaT + Tin
         Power = -self.T_Cp*Tadb*EtaT*W_T*EtaM
-        W_reqd = -PwReq/(self.T_Cp*Tadb*EtaT*EtaM)
+        PwAct = PwReq/EtaM
+        W_reqd = -PwAct/(self.T_Cp*Tadb*EtaT)
         #EtaT_w = f_EtaT(N_T,PRT)
 
         W_bypass = W_T - W_reqd
         BypassRatio = W_bypass/W_T
+        print('PwT',Power, 'EtaT',EtaT,'PRT',PRT,'BPR',BypassRatio,'W_T',W_T,'NT',N_T,'W_Reqd',W_reqd,'WT_Map',WT_Map)
+        return Power,W_reqd,WT_Map,PwAct,N_T,Pout,Tout,PRT,BypassRatio,EtaT
         if BypassRatio < 0:
             print("ERROR: Bypass Ratio must be larger than 0")
-            print('PwT',Power, 'EtaT',EtaT,'PRT',PRT,'BPR',BypassRatio,'W_T',W_T,'NT',N_T,'W_Reqd',W_reqd)
-            return Power,W_reqd,N_T,Pout,Tout,PRT,BypassRatio,EtaT
-    
         else:
-            print('PwT',Power, 'EtaT',EtaT,'PRT',PRT,'BPR',BypassRatio,'W_T',W_T,'NT',N_T,'W_Reqd',W_reqd)
-            return Power,W_reqd,N_T, Pout,Tout,PRT,BypassRatio,EtaT
+            print("")
     
     def PowBal(self,PRC,PwC,PwT,Nc,Nt,Step,PowRes):
         if PRC > self.C_PRC:
             Nc = Nc - Step
             return 1,Nc,Nt
-        elif PRC < self.C_PRC-Step/10000:
+        elif PRC < self.C_PRC-self.S_PRCRes:
             Nc = Nc + Step
             return 1,Nc,Nt
         else:
@@ -151,44 +153,32 @@ def Matching(Stgno,Nc):
     while Cont == 1:
         print("")
         PwC,Nc,P2c,T2c,PRC,EtaC = Stg.CompStage(1,Stg.T_Amb,Stg.P_Amb,Nc,Stg.C_flow,Stg.C_PRC) 
-        Wt,P1t,T1tth = Stg.Combustor(Stg.C_flow,T2c,P2c,Stg.CB_AFR,Stg.C_BLDR,Stg.T_BLDR)
-        PwT,W_reqd,Nt,P2t,T2t,PRT,BPR,EtaT = Stg.TurbStage(1,StgEff,PwC,Stg.CB_t,P1t,Nt,Wt,BackPres) #Pin
+        W_F,Wt,P1t,T1tth = Stg.Combustor(Stg.C_flow,T2c,P2c,Stg.CB_AFR,Stg.C_BLDR,Stg.T_BLDR)
+        PwT,W_reqd,WT_Map,PwAct,Nt,P2t,T2t,PRT,BPR,EtaT = Stg.TurbStage(1,StgEff,PwC,Stg.CB_t,P1t,Nt,Wt,BackPres) #Pin
         Cont,Nc,Nt = Stg.PowBal(PRC,PwC, PwT, Nc, Nt, Stg.S_Step,Stg.S_PowRes)
-    print("")
-    print('Power [kW]: C',PwC,'T',PwT,'RPM',Nc,Nt,'P1t[kPa]',P1t,'P2t',P2t,'P2c',P2c,'T2c[degC]',T2c-273.15,'T2t',T2t-273.15)
-    print('Wt[kg/s]',Wt,'T1t_th[degC]',T1tth-273.15,'PRC',PRC,'PRT',PRT,'Turbine BPR',BPR,"Turbine Pressure Ratio",float(P2c/P1t)) 
-    
-    data = {
-        "Compressor":{
-        "Power[kW]":float(PwC),
-        "rpm":float(Nc),
-        "Outlet Pres":float(P2c),
-        "Outlet Temp":float(T2c-273.15),
-        "PRC":float(PRC),
-        "Efficiency":float(EtaC),
-        "Air Flow[kg/s]":float(Stg.C_flow),
-        "Turbine Pressure Ratio":float(P2c/P1t)
-        },
-        "Turbine":{
-        "Power[kW]":float(PwT),
-        "rpm":float(Nc),
-        "Inlet Pres.[kPa]":float(P1t),
-        "Inlet Temp[K]":float(Stg.CB_t),
-        "Outlet Pres[kPa]":float(P2t),
-        "Outlet Temp[K]":float(T2t-273.15),
-        "PRT":float(PRT),
-        "Efficiency":float(EtaT),
-        "Turbine Flow[kg/s]":float(Wt),
-        "Bypass Ratio":float(BPR),
-        },
-    }
+        
+    dict_c = {"Power[kW]":PwC,"Speed RPM":Nc,"Outlet Pres[kPaA]":P2c,"Outlet Temp[degC]":T2c-273.15,"Pres Ratio PRC":PRC,"Comp Efficiency":EtaC,"Air Flow[kg/s]":Stg.C_flow,"Turbine Pressure Ratio":P2c/P1t}
+    dict_t = {"Enthalpy[kW]":PwT,"Turbine Power[kW]":PwAct, "Speed RPM":Nc,"Inlet Pres.[kPa]":P1t, "Inlet Temp[C]":Stg.CB_t-273.15,"Outlet Pres[kPa]":P2t, "Outlet Temp[degC]":T2t-273.15, "Pres Ratio PRT":PRT, "Turb Efficiency":EtaT,"Turbine Flow[kg/s]":Wt,"Bypass Ratio":BPR}
+    dict_s = {"Fuel Flow [kg/s]":W_F,"Turbine Required Flow[kg/s]":W_reqd,"Turbine Map Flow[kg/s]":WT_Map,"Theoretical T1t[degC]":T1tth-273.15}
+    #list_other = [W_reqd,WT_Map,PwAct,Nt,BPR,Cont,Nc,Nt,W_F,P1t,]
 
-    toml_string = toml.dumps(data)
+    for k,v in dict_c.items():
+        dict_c[k] = float("{:.3f}".format(float(v)))
+    for k,v in dict_t.items():
+        dict_t[k] = float("{:.3f}".format(float(v)))
+    for k,v in dict_s.items():
+        dict_s[k] = float("{:.3f}".format(float(v)))
+
+    dict = {"Compressor":dict_c,
+    "Turbine":dict_t,
+    "Shaft":dict_s}
+
+    toml_string = toml.dumps(dict)
     output_file = "Result_" +Stg.name+".toml"
     with open(output_file,"w") as output:
-        toml.dump(data,output)
+        toml.dump(dict,output)
 
-Matching(1,60000)
+Matching(1,initrpm)
 
 
 
